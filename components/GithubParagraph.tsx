d380aa31ba64567094e5bc0702c682a0d86b7ab9
Jake Laughery <heyitsjake9@gmail.com>
2025-11-13 11:49:34 -0700

    final touches

--- components/GithubParagraph.tsx
+++ components/GithubParagraph.tsx
@@ -3,10 +3,10 @@ export default function GithubParagraph(){
         <p style={{fontSize: "1.2rem", lineHeight: "1.6"}}>
             My final project will be a website built with react that shows users river information around the 6 primary river
             /creeks within 50 miles of Bozeman. Additionally our website will provide the types of flys that have been catching
-            fish recently in that river. I'm excited to build it seeing as Web Development and Fishing are 2 of my current hobbies<br/><br/>
+            fish recently in that river. I'm excited to build it seeing as Web Development and Fishing are 2 of my current hobbies. I think that using react for this project is perfect for dynamically loading the river information and flies<br/><br/>
             View Code on Github: <br/><br/>
             <a 
-                href="https://github.com/ReidVogel8/On-the-Fly"
+                href="https://github.com/Stuskx/my-app-nextjs"
                 style={{color: "blue", textDecoration: "underline"}}
             >
             Github Repo
